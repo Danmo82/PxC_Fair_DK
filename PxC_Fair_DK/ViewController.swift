5df73cd2e56196d279d92c2d44ac3915cc3d5feb
Dan Møller <danmoller@Dans-Mac.local>
2017-08-01 13:22:41 +0200

    Initial Commit

---
+++ PxC_Fair_DK/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PxC_Fair_DK
+//
+//  Created by Dan Møller on 01/08/2017.
+//  Copyright © 2017 Phoenix Contact DK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
